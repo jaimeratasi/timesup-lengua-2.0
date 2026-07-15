5891fb25bcf81e9150bfb09ebda0a6d7e34c9ba5
jaimeratasi <jaimeratasi@gmail.com>
2026-07-15 09:46:00 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,1 +1,262 @@
+const DECKS = {
 
+figuras: {
+nombre: "🎭 Figuras literarias",
+cartas: [
+
+"Metáfora",
+"Símil",
+"Hipérbole",
+"Anáfora",
+"Personificación",
+"Ironía",
+"Paradoja",
+"Antítesis",
+"Elipsis",
+"Hipérbaton",
+"Polisíndeton",
+"Asíndeton",
+"Sinestesia",
+"Epíteto",
+"Oxímoron",
+"Aliteración",
+"Onomatopeya",
+"Gradación",
+"Apóstrofe",
+"Eufemismo",
+"Reticencia",
+"Pleonasmo",
+"Paralelismo",
+"Enumeración",
+"Interrogación retórica",
+"Exclamación retórica",
+"Metonimia",
+"Perífrasis",
+"Alegoría",
+"Prosopografía",
+"Etopeya",
+"Retrato",
+"Quiasmo",
+"Derivación",
+"Calambur",
+"Dilogía"
+
+]
+},
+
+morfologia: {
+nombre:"🧩 Morfología",
+cartas:[
+
+"Sustantivo",
+"Adjetivo",
+"Verbo",
+"Adverbio",
+"Pronombre",
+"Determinante",
+"Preposición",
+"Conjunción",
+"Interjección",
+
+"Prefijo",
+"Sufijo",
+
+"Lexema",
+
+"Morfema",
+
+"Género",
+
+"Número",
+
+"Tiempo verbal",
+
+"Modo verbal",
+
+"Aspecto",
+
+"Persona",
+
+"Raíz",
+
+"Familia léxica",
+
+"Palabra simple",
+
+"Compuesta",
+
+"Derivada",
+
+"Parasintética",
+
+"Sigla",
+
+"Acrónimo",
+
+"Locución"
+
+]
+},
+
+sintaxis:{
+nombre:"🏗️ Sintaxis",
+cartas:[
+
+"Sujeto",
+
+"Predicado",
+
+"Complemento directo",
+
+"Complemento indirecto",
+
+"Complemento circunstancial",
+
+"Atributo",
+
+"Complemento agente",
+
+"Complemento de régimen",
+
+"Complemento predicativo",
+
+"Vocativo",
+
+"Aposición",
+
+"Oración simple",
+
+"Oración compuesta",
+
+"Coordinada",
+
+"Subordinada",
+
+"Yuxtapuesta",
+
+"Nexo",
+
+"Proposición",
+
+"Sintagma nominal",
+
+"Sintagma verbal",
+
+"Sintagma adjetival",
+
+"Sintagma adverbial",
+
+"Sintagma preposicional"
+
+]
+},
+
+literatura:{
+nombre:"📚 Literatura",
+cartas:[
+
+"Romancero",
+
+"Mester de juglaría",
+
+"Mester de clerecía",
+
+"Renacimiento",
+
+"Barroco",
+
+"Ilustración",
+
+"Romanticismo",
+
+"Realismo",
+
+"Naturalismo",
+
+"Modernismo",
+
+"Generación del 98",
+
+"Generación del 27",
+
+"Novecentismo",
+
+"Posguerra",
+
+"Lírica",
+
+"Narrativa",
+
+"Teatro",
+
+"Ensayo",
+
+"Narrador",
+
+"Personaje",
+
+"Espacio",
+
+"Tiempo",
+
+"Argumento",
+
+"Clímax",
+
+"Desenlace",
+
+"Verso",
+
+"Estrofa",
+
+"Rima"
+
+]
+},
+
+ortografia:{
+nombre:"✍️ Ortografía",
+cartas:[
+
+"Tilde diacrítica",
+
+"Tilde",
+
+"Diptongo",
+
+"Hiato",
+
+"Triptongo",
+
+"Palabra aguda",
+
+"Llana",
+
+"Esdrújula",
+
+"Sobreesdrújula",
+
+"Coma",
+
+"Punto",
+
+"Dos puntos",
+
+"Punto y coma",
+
+"Paréntesis",
+
+"Comillas",
+
+"Guion",
+
+"Raya",
+
+"Signos de interrogación",
+
+"Signos de exclamación"
+
+]
+}
+
+};
