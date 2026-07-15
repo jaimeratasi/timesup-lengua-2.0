9413f66512ae6ee90ce3d119314f921a0a9683ce
jaimeratasi <jaimeratasi@gmail.com>
2026-07-15 16:40:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -342,3 +342,26 @@ function pasar(){
     alert("Pasamos");
 
 }
+function iniciarTemporizador(){
+
+    game.timer = game.secondsPerTurn;
+
+    ui.timer.textContent = game.timer;
+
+    game.interval = setInterval(() => {
+
+        game.timer--;
+
+        ui.timer.textContent = game.timer;
+
+        if(game.timer <= 0){
+
+            clearInterval(game.interval);
+
+            alert("Turno terminado");
+
+        }
+
+    },1000);
+
+}
